dbcba51d1fde83e004e10524a369f2b3e70e7a76
xl350r <danhsecure@gmail.com>
2018-07-31 11:10:17 -0700

    added get-free.ps1

---
+++ Get-free.ps1
@@ -0,0 +1,33 @@
+﻿function get-free() { 
+    Param(
+    [parameter(Position=0, mandatory=$True, helpmessage="Drive to check amount of free space")]
+    [ValidateNotNullorEmpty()]
+    [string] $drive, 
+    [parameter(Position=1, mandatory=$False, helpmessage="List of computers to Check")]
+    [ValidateNotNullorEmpty()]
+    [array] $computer
+    )
+    if ($drive.Length -eq 1) {
+        $drive = "DeviceId='$($drive):'"
+    }
+    else {
+        write-host "Please only use a single drive letter."
+    }
+
+    try {
+        if($computer.Length -eq 0) {
+            Get-WmiObject -Class Win32_LogicalDisk -filter $drive | ft SystemName, @{name="Free"; Expression={[math]::round($($_.FreeSpace/1GB),2)}} -auto
+        }
+        else { 
+            foreach ($i in $computer) {
+                Get-WmiObject -Class Win32_LogicalDisk -ComputerName $i -filter $drive | ft SystemName, @{name="Free"; Expression={[math]::round($($_.FreeSpace/1GB),2)}} -auto   
+            }
+        }
+    }
+    Catch {
+    #You should never really run into an exception in normal usage
+    Write-Warning "Failed to create WMI object"
+    Write-Warning $_.exception.message
+    }
+
+}
