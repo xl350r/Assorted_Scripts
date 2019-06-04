076038b7f1bfd8346cde3af6af2c7cbfb5b99527
xl350r <danhsecure@gmail.com>
2019-06-04 11:32:56 -0700

    Fixed issue with arpid function

--- arpid.sh
+++ arpid.sh
@@ -3,14 +3,12 @@
 ##nmap prefixes file installed from brew is at /usr/local/Cellar/nmap/7.60/share/nmap/nmap-mac-prefixes
 ##OS: OSX
 ##
-$nmap_prefixes_file =' location of nmap-mac-prefixes'
+nmap_prefixes_file =' location of nmap-mac-prefixes'
 
 arptable() { arp -a $@ | cut -d " " -f2,4 | tr -d ")(" | tr " " "\t" | egrep -v "^22[4-9]|^2[3-9][-9]|255\t"; }
 
 arpid() {
-MACS=$(arptable | awk -F "\t" '{print $2}' | egrep -v "incomplete" | awk -F ":" '{if (length($1) == 
-1) {a = 0$1}  else {a = $1} if (length($2) == 1) {b =  0$2} else {b = $2} if (length($3) == 1) {c = 
-0$3} else {c = $3} print a,b,c }' | tr -d " "  | tr "\n" ' ' )
+MACS=$(arptable | awk -F "\t" '{print $2}' | egrep -v "incomplete" | awk -F ":" '{if (length($1) == 1) {a = 0$1}  else {a = $1} if (length($2) == 1) {b =  0$2} else {b = $2} if (length($3) == 1) {c = 0$3} else {c = $3} print a,b,c }' | tr -d " "  | tr "\n" ' ' )
 
 for i in $MACS
 do
